e9db0329eaef63fc4053ba5fa84887d2a1c863af
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-08-22 21:42:01 +0800

    adding Joi validation before storing data to db

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const dotenv = require('dotenv');
 dotenv.config();
 const PORT = process.env.PORT;
 
+// Import Routes
+const authRoute = require('./routes/auth');
+
 // Connect to DB
-mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => {
-    console.log("Connected to MongoDB Atlas");
-})
+mongoose.connect(process.env.DB_CONNECT,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("Connected to MongoDB Atlas");
+    })
 
 // body parser
 app.use(express.json());
 
-// Import Routes
-const authRoute = require('./routes/auth');
 
 
 // Route middlewares (add prefix for all routes)
--- routes/auth.js
+++ routes/auth.js
@@ -1,22 +1,45 @@
 const router = require('express').Router();
 const User = require('../models/User');
 
-router.post('/register', async (req, res) => {
-    const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-    });
-    try {
-        const savedUser = await newUser.save();
-        res.send(savedUser);
-    }
-    catch (err) {
-        console.log(err);
-        res.status(400).send("Ooops error happened!");
-    }
 
-})
+// Validation
+const Joi = require('@hapi/joi');
+
+const schema = Joi.object({
+    name: Joi.string()
+        .min(6)
+        .required(),
+    email: Joi.string()
+        .min(6)
+        .required()
+        .email(),
+    password: Joi.string()
+        .min(6)
+        .required(),
+});
+
+router.post('/register', async (req, res) => {
+
+    // validate the data before creating new user
+    const validation = await schema.validate(req.body);
+    res.send(validation);
+
+
+    // const newUser = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password,
+    // });
+    // try {
+    //     const savedUser = await newUser.save();
+    //     res.send(savedUser);
+    // }
+    // catch (err) {
+    //     console.log(err);
+    //     res.status(400).send("Ooops error happened!");
+    // }
+
+});
 
 
 module.exports = router;
