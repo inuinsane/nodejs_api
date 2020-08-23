59f68d4d15c87e141481505816898b2ae13d54b1
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-08-23 23:34:52 +0800

    fixing verifyToken middleware

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -3,13 +3,14 @@ require('dotenv').config();
 
 
 // verify private route
-function auth(req, res, next) {
+module.exports = function (req, res, next) {
     const token = req.header('auth-token');
     if (!token) return res.status(401).send('Access Denied!');
 
     try {
         const verified = jwt.verify(token, process.env.JWT_KEY);
         req.user = verified;
+        next();
 
     } catch (err) {
         res.status(400).send('Invalid Token!');
