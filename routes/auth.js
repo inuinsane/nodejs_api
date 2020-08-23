922633f518cdda98e4b94e11e1ae26573515ac20
Abdullah Ibnu Hasan <38773546+inuinsane@users.noreply.github.com>
2020-08-23 22:33:00 +0800

    Register user route

--- routes/auth.js
+++ routes/auth.js
@@ -1,43 +1,40 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const { registerValidation } = require('../validation');
+const bcrypt = require('bcryptjs');
 
-
-// Validation
-const Joi = require('@hapi/joi');
-
-const schema = Joi.object({
-    name: Joi.string()
-        .min(6)
-        .required(),
-    email: Joi.string()
-        .min(6)
-        .required()
-        .email(),
-    password: Joi.string()
-        .min(6)
-        .required(),
-});
 
 router.post('/register', async (req, res) => {
 
     // validate the data before creating new user
-    const validation = await schema.validate(req.body);
-    res.send(validation);
+    const { error } = registerValidation(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
+    // Check if the user is already in the database
+    const emailExist = await User.findOne({ email: req.body.email });
+    if (emailExist) return res.status(400).send('Email already exists');
 
-    // const newUser = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password,
-    // });
-    // try {
-    //     const savedUser = await newUser.save();
-    //     res.send(savedUser);
-    // }
-    // catch (err) {
-    //     console.log(err);
-    //     res.status(400).send("Ooops error happened!");
-    // }
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+    // Create a new user
+    const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword,
+    });
+    try {
+        const savedUser = await newUser.save();
+        res.status(200).send({
+            user: newUser.id,
+            message: "Success"
+        });
+    }
+    catch (err) {
+        console.log(err);
+        res.status(400).send("Ooops error happened!");
+    }
 
 });
 
